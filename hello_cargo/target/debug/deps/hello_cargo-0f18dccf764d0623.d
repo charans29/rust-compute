8895f96f072343306b6ff2a6905751d1a97f8cdd
charans29 <smodugula@csuchico.edu>
2024-04-09 01:40:12 -0700

    cargo greets

---
+++ hello_cargo/target/debug/deps/hello_cargo-0f18dccf764d0623.d
@@ -0,0 +1,5 @@
+/Users/charans/projects/rust_compute/hello_cargo/target/debug/deps/hello_cargo-0f18dccf764d0623: src/main.rs
+
+/Users/charans/projects/rust_compute/hello_cargo/target/debug/deps/hello_cargo-0f18dccf764d0623.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/charans/projects/rust_compute/hello_cargo/target/debug/hello_cargo: /Users/charans/projects/rust_compute/hello_cargo/src/main.rs
